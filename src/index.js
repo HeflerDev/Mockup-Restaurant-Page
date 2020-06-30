4e3b04e7e44b79ae241ed5f314d2c7541a3dd7b2
HeflerDev <hheflerdevelopment@gmail.com>
2020-06-30 14:50:58 -0300

    Create Mock HTML

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import _ from 'lodash';
 
 function component() {
-  const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-	// Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return console.log('Script is Running');
 
-  return element;
 }
 
-document.body.appendChild(component());
-index.html
+component();
+

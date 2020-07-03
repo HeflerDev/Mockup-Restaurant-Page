592c6361901ff0f4e9042028c94fb1f456138851
HeflerDev <hheflerdevelopment@gmail.com>
2020-07-03 15:55:41 -0300

    Finish Tab Logic

--- src/displayController.js
+++ src/displayController.js
@@ -2,7 +2,7 @@ import renderComponents from './renderComponents'
 
 const displayController = (() => {
 
-    const checkPresenceOf = () => {
+    const _checkPresenceOf = () => {
         let home = document.getElementById('home');
         let about = document.getElementById('about');
         let menu = document.getElementById('menu');
@@ -16,7 +16,28 @@ const displayController = (() => {
             console.log('No Content Parsed');
         }
     }
+    // Parse and Change the button style on click
+    const _swapBtns = (btnId) => {
+        let check = _checkPresenceOf();
 
+        if (check) {
+            if (check.id != 'home');
+                document.getElementById(`${check.id}-btn`).classList.remove('button-active');
+                document.getElementById(`${check.id}-btn`).classList.add('button');
+        }
+
+        let btn = document.getElementById(btnId);
+            btn.classList.remove('button');
+            btn.classList.add('button-active');
+
+    }
+
+    const _swapBg = () => {
+        let check = _checkPresenceOf();
+        document.getElementById(check.id).classList.add('container-active');
+    }
+
+    // Enable Buttons Listener
 	const listenBtns = () => {
 		document.getElementById('home-btn').addEventListener('click', _handleHome);
 		document.getElementById('about-btn').addEventListener('click', _handleAbout);
@@ -24,24 +45,32 @@ const displayController = (() => {
 	}
 
 	const _handleHome = () => {
-        if (checkPresenceOf()) {
-            renderComponents.unRender(checkPresenceOf().id);
-        }
-        renderComponents.renderHome();
+            let check = _checkPresenceOf();
+            _swapBtns('home-btn');
+            if (check) {
+                renderComponents.unRender(check.id);
+            }
+            renderComponents.renderHome();
+            _swapBg();
 	}
 
 	const _handleAbout = () => {
-        if (checkPresenceOf()) {
-		    renderComponents.unRender(checkPresenceOf().id);
+        _swapBtns('about-btn');
+
+        if (_checkPresenceOf()) {
+		    renderComponents.unRender(_checkPresenceOf().id);
         }
         renderComponents.renderAbout();
+        _swapBg();
 	}
 
 	const _handleMenu = () => {
-		if (checkPresenceOf()) {
-            renderComponents.unRender(checkPresenceOf().id);
+        _swapBtns('menu-btn');
+		if (_checkPresenceOf()) {
+            renderComponents.unRender(_checkPresenceOf().id);
         }
         renderComponents.renderMenu();
+        _swapBg();
 	}
 
 	return { listenBtns };
